92bc3b7d2842eddee240d5446c73d3e554b2e939
1a11a <86703816+1a11a@users.noreply.github.com>
2023-10-30 15:47:30 +0530

    ubdate

--- Index.js
+++ Index.js
@@ -1,9 +1,9 @@
 const rotatingText = document.querySelector("#rotating-text");
-const words = ["UI Designer", "UI/UX Developer", "Programmer", "Creator"];
+const words = ["UI/UX Designer", "FrontEnd Developer", "Programmer", "Creator"];
 let currentWordIndex = 0;
 let currentCharacterIndex = 0;
 let isDeleting = false;
-
+  
 function type() {
   const word = words[currentWordIndex];
   const text = isDeleting ? word.slice(0, currentCharacterIndex - 1) : word.slice(0, currentCharacterIndex);
